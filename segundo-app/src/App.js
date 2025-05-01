cd504c005d221a9785d3e815a06a682e826b95cf
leoporto166 <leonardo.porto166@gmail.com>
2025-04-30 21:08:01 -0300

    Formularios React

--- segundo-app/src/App.js
+++ segundo-app/src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
+
+  const [nome, setNome] = useState("");
+  const[email, setEmail] = useState("");
+  const[idade, setIdade] = useState("");
+
+  const[user, setUser] = useState({});
+
+  function clickRegister(event){
+    event.preventDefault()
+    
+    setUser({
+      nome: nome,
+      idade: idade,
+      email: email
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={clickRegister}>
+        <h1>Cadastrando usuario</h1>
+        <label>Nome:</label> <br></br>
+        <input placeholder="Digite seu nome"
+        value = {nome}
+        onChange={(event) => setNome(event.target.value)}></input> <br></br>
+
+        <label>E-mail:</label> <br></br>
+        <input placeholder="Digite seu e-mail"
+        value={email}
+        onChange={ (event) => setEmail(event.target.value)}></input> <br></br>
+
+        <label>Idade:</label> <br></br>
+        <input placeholder="Digite sua Idade"
+        value={idade}
+        onChange={(event) => setIdade(event.target.value)}></input>
+        <br></br>
+
+        <button type="submit">Registrar</button>
+      </form>
+      <br></br>
+      <br></br>
+      <div>
+        <span>Bem vindo: {user.nome}</span>
+        <br></br> 
+        <span>Email: {user.email}</span>
+        <br></br>
+        <span>Idade: {user.idade}</span>
+        <br></br>
+       
+        
+      </div>
     </div>
   );
 }
