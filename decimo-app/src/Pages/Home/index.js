09959f33ad3dcb1c82076d9e246aa2a351471260
leoporto166 <leonardo.porto166@gmail.com>
2025-05-09 21:36:33 -0300

    Porto flix 2

--- decimo-app/src/Pages/Home/index.js
+++ decimo-app/src/Pages/Home/index.js
@@ -1,9 +1,51 @@
 
+import { useEffect, useState } from "react";
+import api from "../../Services/api"
+import { Link } from "react-router-dom";
+import "./home.css"
+
+
+//URL DA API: /movie/now_playing?api_key=3a38a2377454d98eeb8f8f8bf0638850&language=pt-BR
 
 function Home(){
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() =>{
+        async function loadFilmes() {
+            const response = await api.get("movie/now_playing", {params:{
+                api_key: "3a38a2377454d98eeb8f8f8bf0638850",
+                language: "pt-BR",
+                page: 1
+            }
+        })
+
+        console.log(response.data.results.slice(0,10))
+        setFilmes(response.data.results.slice(0,10))
+
+        }
+
+        loadFilmes()
+    }, [])
     return(
-        <div>
-            <h1>Bem vindo a Home</h1>
+        <div className="container">
+            <div className="filmes">
+            {filmes.map((filme) => {
+                return(
+                    
+                    <article className="post" key={filme.id}>
+                        <strong className="title">{filme.title}</strong>
+
+                        <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}className="poster" />
+
+                        <Link to={"/filmes/${filme.id"}>
+                            Acessar
+                        </Link>
+                    </article>
+                
+                )
+            
+            })}
+            </div>
         </div>
     )
 }
---
+++ decimo-app/src/Services/api.js
@@ -0,0 +1,12 @@
+
+import axios from "axios"
+
+//BASE DA URL:https://api.themoviedb.org/3/
+
+//URL DA API: https://api.themoviedb.org/3/movie/now_playing?api_key=3a38a2377454d98eeb8f8f8bf0638850&language=pt-BR
+
+const api = axios.create({
+    baseURL: "https://api.themoviedb.org/3/"
+})
+
+export default api;
