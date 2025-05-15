b80e4dbae502872592a91a87afc280ead37558c0
leoporto166 <leonardo.porto166@gmail.com>
2025-05-15 20:37:55 -0300

    PORTO FLIX 1

--- nono-app/src/Pages/Home/index.js
+++ nono-app/src/Pages/Home/index.js
@@ -1,9 +1,56 @@
 
+import { Link } from "react-router-dom"
 import Api from "../../Services/Api"
+import "./home.css"
+import { useEffect, useState } from "react"
+
+//BASE DA URL:https://api.themoviedb.org/3/
+
+//URL DA API: https://api.themoviedb.org/3/movie/now_playing?api_key=3a38a2377454d98eeb8f8f8bf0638850&language=pt-BR
+
+//URL DA API: /movie/now_playing?api_key=3a38a2377454d98eeb8f8f8bf0638850&language=pt-BR
 
 function Home(){
+    const [filmes, setFilmes] = useState([])
+
+    useEffect(() => {
+        async function loadFilmes() {
+           const response = await Api.get("/movie/now_playing", {
+                params: {
+                api_key: "3a38a2377454d98eeb8f8f8bf0638850",
+                language: "pt-BR",
+                page: 1
+            }
+        })
+
+        console.log(response.data.results.slice(0,20))
+        setFilmes(response.data.results.slice(0,20))
+            
+        }
+
+        loadFilmes();
+    }, [])
     return(
-        <Api />
+        <div className="container">
+            <div className="filmes">
+            {filmes.map((filme) => {
+                return(
+                    
+                    <article className="post" key={filme.id}>
+                        <strong className="title">{filme.title}</strong>
+
+                        <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}className="poster" />
+
+                        <Link to={`/filmes/${filme.id}`}className="home-link">
+                            Acessar
+                        </Link>
+                    </article>
+                
+                )
+            
+            })}
+            </div>
+        </div>
     )
 }
 
--- nono-app/src/Services/Api.js
+++ nono-app/src/Services/Api.js
@@ -1,46 +1,14 @@
-import "./api.css"
+
 
 //BASE DA URL:https://api.themoviedb.org/3/
 
 //URL DA API: https://api.themoviedb.org/3/movie/now_playing?api_key=3a38a2377454d98eeb8f8f8bf0638850&language=pt-BR
 
-import { useEffect, useState } from "react";
-function Api(){
-    const[filmes, setFilmes] = useState([])
+import axios from "axios";
 
-    
-    useEffect(() =>{function loadApi(){
-        let url = "https://api.themoviedb.org/3/movie/now_playing?api_key=3a38a2377454d98eeb8f8f8bf0638850&language=pt-BR"
-
-        fetch(url)
-        .then((resul) => resul.json())
-        .then((json) => {
-            console.log(json)
-            setFilmes(json.results)
-        })
-
-        }
-        loadApi()
-    }, [])
-
-    return(
-        <div className="container">
-            {filmes.map((item) =>{
-                return(
-                <article key={item.id} className="post">
-                    <strong className="title">{item.original_title}</strong>
-
-                    <img src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} className="poster" />
-
-                    <button className="button">
-                        Acessar
-                    </button>
-                </article>
-                )
-            })}
-        </div>
-    )
-
-}
+// Cria uma instância do Axios com a base da URL da API do TMDB
+const Api = axios.create({
+    baseURL: "https://api.themoviedb.org/3/"
+});
 
 export default Api;
