86eb4bb5d10a97a031acc17eaa91b2af47dd0e6b
leoporto166 <leonardo.porto166@gmail.com>
2025-05-20 19:46:25 -0300

    FireBase

---
+++ app-doze/src/FirebaseConnection.js
@@ -0,0 +1,19 @@
+
+import {initializeApp} from "firebase/app"
+import {getFirestore}  from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCeuMN9O-lCdCJrOhJw-1M5g7goW1fF69U",
+  authDomain : "curso-1bf3d.firebaseapp.com" , 
+  projectId : "course-1bf3d" , 
+  storageBucket: "curso-1bf3d.firebasestorage.app",
+  messagingSenderId: "7117202146",
+  appId: "1:7117202146:web:b66002022ec72eeec1b4bb",
+  measurementId: "G-81XYZZ8802"
+};
+
+const firebaseapp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseapp);
+
+export {db};
