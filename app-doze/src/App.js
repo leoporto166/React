7b898f142c748d4be541084543d044bfd7e28274
leoporto166 <leonardo.porto166@gmail.com>
2025-05-22 16:11:03 -0300

    att

--- app-doze/src/App.js
+++ app-doze/src/App.js
@@ -1,10 +1,67 @@
+import { useState } from "react";
 import {db} from "./FirebaseConnection"
-
+import { doc, getDoc, addDoc, collection } from "firebase/firestore";
 function App() {
+  const [titulo, setTitulo] = useState("")
+  const [autor, setAutor] = useState("")
+
+  async function Salvar() {
+    
+    await addDoc(collection(db, "posts"), {
+      titulo: titulo,
+      autor: autor,
+    })
+
+    .then(() => {
+      console.log("CADASTRADO!")
+      setTitulo("")
+      setAutor("")
+    })
+    .catch((error) =>{
+      console.log(`ERRO ${error}`)
+    })
+  }
+
+  async function Buscar() {
+    const Ref = doc(db,"posts", "1234")
+
+    await getDoc(Ref)
+
+    .then((snapshot) => {
+      setAutor(snapshot.data().autor)
+      setTitulo(snapshot.data().titulo)
+    })
+    .catch(() =>{
+      console.log("Erro ao buscar")
+    })
+    
+  }
   return (
     <div>
       <h1>React JS + Firebase</h1>
+      <div className="form">
+        <label>Titulo</label>
+        <input
+        type="text"
+        placeholder="Digite o titulo"
+        value={titulo}
+        onChange={(event) => setTitulo(event.target.value)}
+        />      
+
+        <label>Autor</label>
+        <input 
+        type="text"
+        placeholder="Digite o autor"
+        value={autor}
+        onChange={(event) => setAutor(event.target.value)}
+        />
+
+        <button onClick={Salvar}>Cadastrar</button>
+        <button onClick={Buscar}>Buscar</button>
+      </div>
     </div>
+
+    
   );
 }
 
--- app-doze/src/FirebaseConnection.js
+++ app-doze/src/FirebaseConnection.js
@@ -1,11 +1,10 @@
-
-import {initializeApp} from "firebase/app"
-import {getFirestore}  from "firebase/firestore"
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCeuMN9O-lCdCJrOhJw-1M5g7goW1fF69U",
-  authDomain : "curso-1bf3d.firebaseapp.com" , 
-  projectId : "course-1bf3d" , 
+  authDomain: "curso-1bf3d.firebaseapp.com",
+  projectId: "curso-1bf3d", // <-- corrigido aqui
   storageBucket: "curso-1bf3d.firebasestorage.app",
   messagingSenderId: "7117202146",
   appId: "1:7117202146:web:b66002022ec72eeec1b4bb",
@@ -13,7 +12,6 @@ const firebaseConfig = {
 };
 
 const firebaseapp = initializeApp(firebaseConfig);
-
 const db = getFirestore(firebaseapp);
 
-export {db};
+export { db };
