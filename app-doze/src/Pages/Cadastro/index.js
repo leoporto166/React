0e6dc870a01591c4ab6213a68edf2206dd68beaa
leoporto166 <leonardo.porto166@gmail.com>
2025-05-23 19:20:51 -0300

    LOGIN, LOGOUT, PERMANENCIA

--- app-doze/src/Pages/Cadastro/index.js
+++ app-doze/src/Pages/Cadastro/index.js
@@ -1,11 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {auth} from "../../FirebaseConnection"
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
 import "./cadastro.css"
 
 function Cadastro() {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
+  const [user, setUser] = useState(false)
+  const [detalhes, setDetalhes] = useState({})
+  
+  useEffect(() => {
+    async function checkLogin() {
+      onAuthStateChanged(auth, (user) => {
+        if(user){
+          console.log(user)
+          setUser(true)
+          setDetalhes({
+            uid: user.uid,
+            email: user.email,
+          })
+          
+        } else{
+          setDetalhes({})
+          setUser(false)
+        }
+      })
+    }
+
+    checkLogin();
+  }, [])
 
   async function Cadastrar() {
     const Ref = createUserWithEmailAndPassword(auth, email, senha)
@@ -25,9 +48,52 @@ function Cadastro() {
     
   }
 
+  async function Logar() {
+    await signInWithEmailAndPassword(auth, email, senha)
+
+    .then((value) =>{
+      console.log("Usuário logado!")
+      console.log(value.user)
+
+      setDetalhes({
+        uid: value.user.uid,
+        email: value.user.email,
+      })
+      setUser(true)
+
+      setEmail("")
+      setSenha("")
+    })
+    .catch((error) => {
+      console.log(`ERRO: ${error}`)
+    })
+    
+  }
+
+  async function logOut() {
+    await signOut(auth)
+    .then(() =>{
+      alert("Deslogado!")
+      setUser(false)
+      setDetalhes({})
+    })
+    .catch((error) =>{
+      console.log(`ERRO: ${error}`)
+    })
+    
+  }
+
   return (
     <div className="cadastro-form">
       <h1>Cadastro</h1>
+
+      {user &&(
+        <div>
+          <strong>Bem vindo(a)</strong> <br/>
+          <span>ID: {detalhes.uid} - Email:{detalhes.email}</span><br/> <br/>
+          <button onClick={logOut}>Logout</button> <br/>
+        </div>
+      )}
 
     <label>
         Email:
@@ -52,6 +118,7 @@ function Cadastro() {
       
 
       <button onClick={Cadastrar}>Cadastrar</button>
+      <button onClick={Logar}>Logar</button>
     </div>
   );
 }
